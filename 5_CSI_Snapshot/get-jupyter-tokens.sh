ae27e45f7292b91403f6da7517f6e5e73df1ac4c
Pablo Garcia Arevalo <pgarevalo@gmail.com>
2020-06-07 21:02:58 +0200

    Delete get-jupyter-tokens.sh

--- 5_CSI_Snapshot/get-jupyter-tokens.sh
+++
@@ -1,9 +0,0 @@
-oc get pod -l "lab=jupyter" -n 4-webinar -o jsonpath='{.items[0].metadata.name}'; echo "\n"; oc exec -i -t $(kubectl get pod -l "lab=jupyter" -n 4-webinar -o jsonpath='{.items[0].metadata.name}') -n 4-webinar -- jupyter notebook list
-
-echo ""
-
-oc get pod -l "lab=jupyter" -n 4-webinar -o jsonpath='{.items[1].metadata.name}'; echo "\n"; oc exec -i -t $(kubectl get pod -l "lab=jupyter" -n 4-webinar -o jsonpath='{.items[1].metadata.name}') -n 4-webinar -- jupyter notebook list
-
-echo ""
-
-oc get pod -l "lab=jupyter" -n 4-webinar -o jsonpath='{.items[2].metadata.name}'; echo "\n"; oc exec -i -t $(kubectl get pod -l "lab=jupyter" -n 4-webinar -o jsonpath='{.items[2].metadata.name}') -n 4-webinar -- jupyter notebook list
